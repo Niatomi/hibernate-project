843a6b4a45207d3ec333aee9f68ab5ecb7727610
Niatomi <playervoker@gmail.com>
2021-12-03 19:45:27 +0400

    Added fields into class: ID Name Author class Reader class

---
+++ src/main/java/ru/niatomi/hibernate/model/persistence/Article.java
@@ -0,0 +1,27 @@
+package ru.niatomi.hibernate.model.persistence;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "author_table")
+@Getter
+@Setter
+@RequiredArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@ToString
+public class Article {
+
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    private String name;
+
+    private Author author;
+
+    private Reader reader;
+
+
+}
