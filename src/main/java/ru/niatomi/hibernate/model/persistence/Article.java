20e441399230721fb5149f4e7cb8fc7b179126b1
Niatomi <playervoker@gmail.com>
2021-12-07 19:25:10 +0400

    Added couple Annotations

--- src/main/java/ru/niatomi/hibernate/model/persistence/Article.java
+++ src/main/java/ru/niatomi/hibernate/model/persistence/Article.java
@@ -1,26 +1,30 @@
 package ru.niatomi.hibernate.model.persistence;
 
 import lombok.*;
+import org.hibernate.annotations.GeneratorType;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
-@Table(name = "article_table")
+@Table(name = "articles")
 @Getter
 @Setter
 @RequiredArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @ToString
 public class Article {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @EqualsAndHashCode.Include
     private Long id;
 
+    @Column(name = "name", length = 63, nullable = false)
     private String name;
 
+    @Column(name = "author", length = 63, nullable = false)
     private Author author;
 
+    @Column(name = "reader", length = 63, nullable = false)
     private Reader reader;
 
 
