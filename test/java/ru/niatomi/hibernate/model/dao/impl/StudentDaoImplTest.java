d3b206e4e95360f9abc84fcf7a6e1c1fab0d14cc
niatomi <playervoker@gmail.com>
2021-12-12 16:59:42 +0400

    FindAll check add.

--- test/java/ru/niatomi/hibernate/model/dao/impl/StudentDaoImplTest.java
+++ test/java/ru/niatomi/hibernate/model/dao/impl/StudentDaoImplTest.java
@@ -4,6 +4,9 @@ import org.junit.Test;
 import ru.niatomi.hibernate.model.dao.StudentDao;
 import ru.niatomi.hibernate.model.persistence.Student;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.Assert.*;
@@ -20,5 +23,20 @@ public class StudentDaoImplTest {
 
         assertTrue(actual.isPresent());
         assertEquals(expected, actual.get());
+    }
+
+    @Test
+    public void findAll() {
+        List<Student> expected = Arrays.asList(STUDENT_1);
+
+        List<Student> actual = dao.findAll();
+
+        assertTrue(!actual.isEmpty());
+
+        assertTrue(actual.size() == expected.size());
+
+        for (int i = 0; i < actual.size(); i++) {
+            assertEquals(expected.get(i), actual.get(i));
+        }
     }
 }
