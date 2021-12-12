6e20133383f70873ce62c012078a0d0866e06277
niatomi <playervoker@gmail.com>
2021-12-12 12:24:26 +0400

    All methods transfered to their own classes

--- src/main/java/ru/niatomi/hibernate/Launcher.java
+++ src/main/java/ru/niatomi/hibernate/Launcher.java
@@ -1,8 +1,32 @@
 package ru.niatomi.hibernate;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import ru.niatomi.hibernate.model.persistence.Address;
+import ru.niatomi.hibernate.model.persistence.Student;
+import ru.niatomi.hibernate.util.SessionUtil;
+
+import java.util.List;
+
 public class Launcher {
 
     public static void main(String[] args) {
 
+        Launcher launcher = new Launcher();
+
+        Student student = new Student();
+        student.setFirstName("ya");
+        student.setLastName("balbes");
+
+        Address adress = new Address();
+        adress.setName("kolotushkina 2");
+
+//        student.setAdress(adress);
+//        launcher.saveUser(student);
+
+
     }
+
+
+
 }
