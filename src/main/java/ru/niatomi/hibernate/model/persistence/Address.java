754faddd698f79889f30fa97a776b27a2988f4ae
niatomi <playervoker@gmail.com>
2021-12-12 17:02:48 +0400

    Edited version of GeneratedValue to Identity in strategy type to

--- src/main/java/ru/niatomi/hibernate/model/persistence/Address.java
+++ src/main/java/ru/niatomi/hibernate/model/persistence/Address.java
@@ -15,7 +15,7 @@ public class Address {
 
     @Id
     @EqualsAndHashCode.Include
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "street")
