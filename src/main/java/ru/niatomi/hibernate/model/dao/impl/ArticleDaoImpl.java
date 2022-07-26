2c5cd59651261921e399a02aa2a5fcfe3828ae9a
Niatomi <playervoker@gmail.com>
2022-07-27 00:32:04 +0400

    Initial commit

---
+++ src/main/java/ru/niatomi/hibernate/model/dao/impl/ArticleDaoImpl.java
@@ -0,0 +1,37 @@
+package ru.niatomi.hibernate.model.dao.impl;
+
+import ru.niatomi.hibernate.model.dao.ArticleDao;
+import ru.niatomi.hibernate.model.persistence.Article;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @author niatomi
+ */
+public class ArticleDaoImpl implements ArticleDao {
+    @Override
+    public Optional<Article> findById(Long key) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Article> findAll() {
+        return null;
+    }
+
+    @Override
+    public Article save(Article article) {
+        return null;
+    }
+
+    @Override
+    public Optional<Article> update(Long key, Article article) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Article> deleteById(Long key) {
+        return Optional.empty();
+    }
+}
