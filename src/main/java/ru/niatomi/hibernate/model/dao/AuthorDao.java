be187ca90e909dc3fa6584d0754b10c48208e493
Niatomi <playervoker@gmail.com>
2022-07-27 00:38:15 +0400

    Add finding authors by count of articles

--- src/main/java/ru/niatomi/hibernate/model/dao/AuthorDao.java
+++ src/main/java/ru/niatomi/hibernate/model/dao/AuthorDao.java
@@ -2,8 +2,13 @@ package ru.niatomi.hibernate.model.dao;
 
 import ru.niatomi.hibernate.model.persistence.Author;
 
+import java.util.List;
+
 /**
  * @author niatomi
  */
 public interface AuthorDao extends GenericDao<Author, Long> {
+
+    List<Author> getAuthorsWithArticlesHigherThan(Long countOfArticles);
+
 }
