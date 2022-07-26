fc1cdbe471d0d412ea776697c801b13d7590dd15
Niatomi <playervoker@gmail.com>
2022-07-26 18:42:39 +0400

    Deleted

--- src/main/java/ru/niatomi/hibernate/model/dao/StudentDao.java
+++
@@ -1,9 +0,0 @@
-package ru.niatomi.hibernate.model.dao;
-
-import ru.niatomi.hibernate.model.persistence.Student;
-
-import java.util.List;
-
-public interface StudentDao extends GenericDao<Student, Long>{
-    List<Student> findByStartsFirstName(String starts);
-}
