c5bef1d62c18e14838f7cc92fee623c81610f21e
Niatomi <playervoker@gmail.com>
2022-07-27 23:27:34 +0400

    Add public variables for table names in native implementation

--- src/main/java/ru/niatomi/hibernate/model/persistence/Reader.java
+++ src/main/java/ru/niatomi/hibernate/model/persistence/Reader.java
@@ -6,7 +6,7 @@ import javax.persistence.*;
 import java.util.List;
 
 @Entity
-@Table(name = "reader_table")
+@Table(name = Reader.READER_TABLE_NAME)
 @Getter
 @Setter
 @RequiredArgsConstructor
@@ -14,21 +14,26 @@ import java.util.List;
 @ToString
 public class Reader {
 
+    public static final String READER_TABLE_NAME = "reader_table";
+    public static final String READER_TABLE_FIRST_NAME = "first_name";
+    public static final String READER_TABLE_SECOND_NAME = "second_name";
+    public static final String READER_TABLE_MAIL = "mail";
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @EqualsAndHashCode.Include
     private Long id;
 
-    @Column(name = "first_name", length = 32)
+    @Column(name = READER_TABLE_FIRST_NAME, length = 32)
     @EqualsAndHashCode.Include
     private String firstName;
 
-    @Column(name = "second_name", length = 32)
+    @Column(name = READER_TABLE_SECOND_NAME, length = 32)
     @EqualsAndHashCode.Include
     private String secondName;
 
     @EqualsAndHashCode.Include
-    @Column(name = "mail", length = 32)
+    @Column(name = READER_TABLE_MAIL, length = 32)
     private String mail;
 
     @ManyToMany
